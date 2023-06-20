f10dbe90bc5e205296dea010e95cdd85bcc4cddd
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-06-20 18:02:47 +0545

    feat: ui of home page added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "../../public/hero.png";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital product.
+        </h1>
+        <p className={styles.desc}>
+          Turnig your idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.btn_home}>See Our Works</button>
+      </div>
+
+      <div className={styles.item}>
+        <Image src={Hero} alt="hero image" className={styles.img} />
+      </div>
+    </div>
+  );
 }
