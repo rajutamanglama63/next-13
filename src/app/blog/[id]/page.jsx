a3730de7af48d98d1df562763accb1e490f694f5
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-06-22 13:23:44 +0545

    feat: ui of single blog done

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,76 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const BlogPost = () => {
-  return <div className={styles.container}>BlogPost</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem Ipsum is simply dummy text of the printing.
+          </h1>
+          <p className={styles.desc}>
+            Many desktop publishing packages and web page editors now use Lorem
+            Ipsum as their default model text, and a search for 'lorem ipsum'
+            will uncover many web sites still in their infancy. Various versions
+            have evolved over the years, sometimes by accident, sometimes on
+            purpose (injected humour and the like).
+          </p>
+
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/14361161/pexels-photo-14361161.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="avatar"
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>John Doe</span>
+          </div>
+        </div>
+
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/17019454/pexels-photo-17019454/free-photo-of-city-art-street-building.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt="art"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+          it over 2000 years old. Richard McClintock, a Latin professor at
+          Hampden-Sydney College in Virginia, looked up one of the more obscure
+          Latin words, consectetur, from a Lorem Ipsum passage, and going
+          through the cites of the word in classical literature, discovered the
+          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+          Evil) by Cicero, written in 45 BC. This book is a treatise on the
+          theory of ethics, very popular during the Renaissance. The first line
+          of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
+          section 1.10.32.
+          <br />
+          <br />
+          <p>
+            The standard chunk of Lorem Ipsum used since the 1500s is reproduced
+            below for those interested.
+          </p>
+          <br />
+          <p>
+            It uses a dictionary of over 200 Latin words, combined with a
+            handful of model sentence structures, to generate Lorem Ipsum which
+            looks reasonable. The generated Lorem Ipsum is therefore always free
+            from repetition, injected humour, or non-characteristic words etc.
+          </p>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link className={styles.container} href="/blog/123">
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/908713/pexels-photo-908713.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+
+        <div className={styles.content}>
+          <h1 className={styles.title}>Tifle</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+
+      <Link className={styles.container} href="/blog/123">
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/908713/pexels-photo-908713.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+
+        <div className={styles.content}>
+          <h1 className={styles.title}>Tifle</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+
+      <Link className={styles.container} href="/blog/123">
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/908713/pexels-photo-908713.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+
+        <div className={styles.content}>
+          <h1 className={styles.title}>Tifle</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
