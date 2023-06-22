9e781877665558781886045a1826465c33a68177
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-06-22 12:37:25 +0545

    feat: inner routes of category designed

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,66 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
-const Category = () => {
-  return <div className={styles.container}>Category</div>;
+const Category = ({ params }) => {
+  console.log(params);
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#" text="See more" />
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/1194420/pexels-photo-1194420.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="container image"
+          />
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#" text="See more" />
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/1194420/pexels-photo-1194420.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="container image"
+          />
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button url="#" text="See more" />
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/1194420/pexels-photo-1194420.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="container image"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
