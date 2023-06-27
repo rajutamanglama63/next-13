fab41aa6bd65ac696fa6025c643edd64a7e52e82
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-06-27 18:03:21 +0545

    feat: manual user registration

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,31 @@
+import User from "@/models/User";
+import connectDB from "@/utils/db";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export const POST = async (request) => {
+  const { name, email, password } = await request.json();
+  console.log("name: ", name, "email: ", email, "password: ", password);
+
+  await connectDB();
+
+  const hassedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({
+    name,
+    email,
+    password: hassedPassword,
+  });
+
+  try {
+    await newUser.save();
+    return new NextResponse("User has been created", {
+      status: 201,
+    });
+  } catch (error) {
+    console.log("registration error: ", error);
+    return new NextResponse(error.message, {
+      status: 500,
+    });
+  }
+};
--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,12 +1,42 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
+  const [err, setErr] = useState(false);
+
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      res.status === 201 &&
+        router.push("/dashboard/login?success=Account has been created");
+    } catch (error) {
+      console.log("error: ", error);
+      setErr(true);
+    }
+  };
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="username"
@@ -25,8 +55,11 @@ const Register = () => {
           className={styles.input}
           required
         />
-        <button className={styles.button}>Register</button>
+        <button type="submit" className={styles.button}>
+          Register
+        </button>
       </form>
+      {err && "Something went worng!"}
       <Link href="/dashboard/login">Login with existing account</Link>
     </div>
   );
